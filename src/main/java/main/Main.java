edcd63fcca02ac59ce7a49f194db7649ff6671e6
Dmitriy Sadkov <dmitriy_sadkov@mail.ru>
2019-11-19 10:35:59 +0300

    Working Server version1.0

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,6 +4,8 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import servlets.GetServlet;
+import servlets.SignInServlet;
+import servlets.SignUpServlet;
 
 import java.util.logging.Logger;
 
@@ -11,9 +13,13 @@ public class Main {
     public static void main(String[] args) throws Exception {
         //создаем сервлет для обработки запроса
         GetServlet getServlet = new GetServlet();
+        SignInServlet signInServlet = new SignInServlet();
+        SignUpServlet signUpServlet = new SignUpServlet();
         //создаем хандлер для перехвата запроса и передаем в него сервлет
         ServletContextHandler handler = new ServletContextHandler(ServletContextHandler.SESSIONS);
         handler.addServlet(new ServletHolder(getServlet),"/mirror");
+        handler.addServlet(new ServletHolder(signInServlet),"/signin");
+        handler.addServlet(new ServletHolder(signUpServlet),"/signup");
         //создаем сервер из джетти на порту 8080 и устанавливаем для него хандлер
         Server server = new Server(8080);
         server.setHandler(handler);
